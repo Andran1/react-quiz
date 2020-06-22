2bde452aeee69944e1fee2732c489816b2da22dd
Andran1 <Andran1@users.noreply.github.com>
2020-06-22 15:45:15 +0400

    Updates

---
+++ precache-manifest.1cec861cf308efca3a7b5ceeaa46c0f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aac7ed9cc7b698101e22f4c9f3d6e35",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "7e5c60640c46832f6466",
+    "url": "/react-quiz/static/css/main.c7e46b5b.chunk.css"
+  },
+  {
+    "revision": "d255255f7518c6afc3af",
+    "url": "/react-quiz/static/js/2.11a3161a.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/react-quiz/static/js/2.11a3161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5c60640c46832f6466",
+    "url": "/react-quiz/static/js/main.6b52f51d.chunk.js"
+  },
+  {
+    "revision": "65612cb1b7e4f09e4ac4",
+    "url": "/react-quiz/static/js/runtime-main.dbeed73e.js"
+  }
+]);
